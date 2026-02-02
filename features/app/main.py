ed9e0acefc181756d3fb5792ff37678151843540
Mohith Bharath <124501905+MB-88@users.noreply.github.com>
2026-02-03 02:26:16 +0530

    Add new CORS origin for cafeteria service
    Added frontend url for cloud deployment

--- features/app/main.py
+++ features/app/main.py
@@ -63,7 +63,7 @@ app.add_middleware(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL, "http://localhost:3000"],
+    allow_origins=[FRONTEND_URL, "http://localhost:3000","https://cafeteria-service.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
